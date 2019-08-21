a583d684e4af3910f18d7a669d7dd0caaa173234
Roland Jiang <19jiangr@abschools.org>
2019-08-21 12:09:29 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,19 +16,25 @@ client.on("ready", () => {
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
   // Example of changing the bot's playing game to something useful. `client.user` is what the
   // docs refer to as the "ClientUser".
-  client.user.setPresence(`Serving ${client.guilds.size} servers`);
+  client.user.setPresence({
+        game: {
+            name: 'Use +help',
+            type: "Playing",
+            url: "https://discordapp.com/"
+        }
+    });
 });
 
 client.on("guildCreate", guild => {
   // This event triggers when the bot joins a guild.
   console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-  client.user.setPresence(`Serving ${client.guilds.size} servers`);
+  //client.user.setPresence(`Serving ${client.guilds.size} servers`);
 });
 
 client.on("guildDelete", guild => {
   // this event triggers when the bot is removed from a guild.
   console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-  client.user.setPresence(`Serving ${client.guilds.size} servers`);
+  //client.user.setPresence(`Serving ${client.guilds.size} servers`);
 });
 
 
@@ -49,7 +55,7 @@ client.on("message", async message => {
   // args = ["Is", "this", "the", "real", "life?"]
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
-  client.log('Detected command');
+  console.log('Detected command');
   
   // Let's go with a few common example commands! Feel free to delete or change those.
   
